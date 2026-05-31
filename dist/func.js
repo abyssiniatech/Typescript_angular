8d5b71dfcb3f06a1149e99bc22f2eef586310814
abyssiniatech <surafelmengist2015@gmail.com>
2026-05-31 19:47:12 +0300

    add function:

---
+++ dist/func.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function sum(num1, num2) {
+    return num1 + num2;
+}
+const result = sum(5, 10);
+console.log('====================================');
+console.log(result);
+console.log('====================================');
---
+++ src/func.ts
@@ -0,0 +1,7 @@
+function sum(num1: number, num2: number): number {
+    return num1 + num2;
+}
+const result = sum(5, 10);
+console.log('====================================');
+console.log(result);
+console.log('====================================');
