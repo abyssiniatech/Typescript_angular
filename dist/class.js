c503b55a71a7d28a536f1723c5d3bd47a3b45317
abyssiniatech <surafelmengist2015@gmail.com>
2026-06-02 11:47:58 +0300

    add class :

--- dist/class.js
+++ dist/class.js
@@ -18,3 +18,34 @@ var res = name("welcome ");
 console.log('====================================');
 console.log(name);
 console.log('====================================');
+class Employee {
+    name;
+    id;
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+    }
+}
+const employee1 = new Employee("John Doe", 123);
+console.log('====================================');
+console.log(employee1);
+console.log('====================================');
+// the second class 
+class Car {
+    make;
+    year;
+    color;
+    constructor(make, year, color) {
+        this.make = make;
+        this.year = year;
+        this.color = color;
+    }
+    move() {
+        console.log(`${this.make} is moving.`);
+    }
+}
+const car1 = new Car("Toyota", 2020, "Red");
+console.log('====================================');
+console.log(car1);
+car1.move();
+console.log('====================================');
--- src/class.ts
+++ src/class.ts
@@ -23,3 +23,41 @@ var res=name("welcome ")
 console.log('====================================');
 console.log(name);
 console.log('====================================');
+
+
+class Employee {
+    name: string;
+    id: number;
+
+    constructor(name: string, id: number) {
+        this.name = name;
+        this.id = id;
+    }
+
+}
+const employee1 = new Employee("John Doe", 123);
+console.log('====================================');
+console.log(employee1);
+console.log('====================================');
+
+
+
+// the second class 
+class Car{
+    make:string;
+    year:number;
+    color:string;
+    constructor(make:string,year:number,color:string){
+        this.make=make;
+        this.year=year;
+        this.color=color;
+    }
+    move():void{
+        console.log(`${this.make} is moving.`);
+    } 
+}
+const car1=new Car("Toyota",2020,"Red")
+console.log('====================================');
+console.log(car1);
+car1.move();
+console.log('====================================');
